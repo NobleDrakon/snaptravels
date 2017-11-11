c0624a01b4f5e01f43670a38429053aa11527a09
Ross Gaskell <rossgaskell@gmail.com>
2017-11-11 17:39:02 +0000

    Initial commit
    Database connection and config files

---
+++ inc/config-sample.php
@@ -0,0 +1,11 @@
+<?php
+
+// SAMPLE CONFIG FILE
+// To Use: enter connect
+
+// MySQL CONNECTION
+// Define database connection details
+define('DBSRV', '');
+define('DBUSER', '');
+define('DBPASS', '');
+define('DBNAME', '');
---
+++ inc/connection.php
@@ -0,0 +1,19 @@
+<?php
+// Import DB connection constants
+require_once('config.php');
+
+// MySQL DATABASE CONNECTION
+
+try {
+    $conn = new PDO("mysql:host=" . DBSRV . ";dbname=" . DBNAME, DBUSER, DBPASS);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+}
+	
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
